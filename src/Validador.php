842ea4bddd0436d747eb536c0dfb652bce8be12f
Mannuel Victor Limeira <mannueldp@gmail.com>
2023-12-02 16:37:26 -0300

    Add main logic validating brackets

--- src/Validador.php
+++ src/Validador.php
@@ -6,8 +6,62 @@ namespace SuportesBalanceados;
 
 class Validador
 {
-  public function validate(string $value): bool
+  public function validar(string $texto): bool
   {
+    $textoSanitizado = $this->sanitizarEntrada($texto);
+    return $this->ehValido($textoSanitizado);
+  }
+
+  private function sanitizarEntrada(string $texto): string
+  {
+    $padraoCaracteresPermitidos = '/[^\[\{\(\)\}\]]/i';
+    return preg_replace($padraoCaracteresPermitidos, '', $texto);
+  }
+
+  private function ehValido(string $texto): bool
+  {
+    if (!strlen($texto)) {
+      return true;
+    }
+
+    $pilhaAberturas = [];
+    foreach (str_split($texto) as $caractere) {
+      if ($this->ehAbertura($caractere)) {
+        array_push($pilhaAberturas, $caractere);
+        continue;
+      }
+
+      $ultimaAbertura = array_pop($pilhaAberturas);
+      if (!$this->ehParValido($ultimaAbertura, $caractere)) {
+        return false;
+      }
+    }
+
+    if (!empty($pilhaAberturas)) {
+      return false;
+    }
+
     return true;
+  }
+
+  private function ehAbertura(string $caractere): bool|int
+  {
+    $padraoAberturas = '/[\[|\{|\(]/i';
+    return preg_match($padraoAberturas, $caractere);
+  }
+
+  private function ehParValido(string|null $abertura, string $fechamento): bool
+  {
+    if (!$abertura) {
+      return false;
+    }
+
+    $lookupParesAberturas = [
+      '{' => '}',
+      '[' => ']',
+      '(' => ')'
+    ];
+
+    return $lookupParesAberturas[$abertura] == $fechamento;
   }
 }
