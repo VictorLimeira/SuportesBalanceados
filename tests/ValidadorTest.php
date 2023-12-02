a9f1eae91648e462afdbd940fa5f1a382d5aff12
Mannuel Victor Limeira <mannueldp@gmail.com>
2023-12-02 16:38:00 -0300

    Add automatic tests

--- tests/ValidadorTest.php
+++ tests/ValidadorTest.php
@@ -15,6 +15,11 @@ class ValidadorTest extends TestCase
       'case 2'  => ['[{()}](){}', true],
       'case 3'  => ['[]{()', false],
       'case 4'  => ['[{)]', false],
+      'case 5'  => ['[{(', false],
+      'case 6'  => [']})', false],
+      'case 7'  => ['[a658{1h)35a]', false],
+      'case 8'  => ['(ola){entre}[válidos]', true],
+      'case 9'  => ['', true],
     ];
   }
 
@@ -22,6 +27,6 @@ class ValidadorTest extends TestCase
   public function test_it_should_validate_simple_brackets(string $value, bool $expectedReturn): void
   {
     $validador = new Validador();
-    self::assertEquals($validador->validate($value), $expectedReturn);
+    self::assertEquals($validador->validar($value), $expectedReturn);
   }
 }
